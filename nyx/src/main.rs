0f118efac94096e46e200d3f8867d6fad94b5c49
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-10-14 16:07:42 -0700

    Basic macroquad example project

--- nyx/src/main.rs
+++ nyx/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Nyx".to_owned(),
+        window_width: 800,
+        window_height: 600,
+        window_resizable: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(LIGHTGRAY);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("HELLO", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
 }
