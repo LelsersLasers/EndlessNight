d4153fcc53e44a59180faf3a0067151c115920fe
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-10-19 14:40:21 -0700

    Fixing double rendering with dithering (Y is flipped for the lighting)

--- nyx/src/camera.rs
+++ nyx/src/camera.rs
@@ -1,9 +1,9 @@
-use crate::vector::Vector2D;
-use macroquad::prelude as mq;
+// use crate::vector::Vector2D;
+// use macroquad::prelude as mq;
 
-pub struct Camera {
-    pub pt: Vector2D,
-    pub offset: Vector2D, // for black bars
-    pub ratio: f32,       // width / height
-    pub scale: f32,
-}
+// pub struct Camera {
+//     pub pt: Vector2D,
+//     pub offset: Vector2D, // for black bars
+//     pub ratio: f32,       // width / height
+//     pub scale: f32,
+// }
--- nyx/src/main.rs
+++ nyx/src/main.rs
@@ -2,7 +2,7 @@ mod camera;
 mod light;
 mod vector;
 
-use crate::{camera::Camera, light::Light, vector::Vector2D};
+use crate::{/*camera::Camera,*/ light::Light, vector::Vector2D};
 
 use macroquad::prelude as mq;
 
@@ -10,6 +10,9 @@ const START_WIDTH: i32 = 1440;
 const START_HEIGHT: i32 = 810;
 
 const DITHER: [i32; 16] = [0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5];
+const DITHER_SIZE: u32 = 4;
+
+
 
 fn window_conf() -> mq::Conf {
     mq::Conf {
@@ -27,32 +30,32 @@ async fn main() {
     let mut camera = mq::Camera2D::from_display_rect(mq::Rect::new(0.0, 0.0, START_WIDTH as f32, START_HEIGHT as f32));
     camera.render_target = Some(mq::render_target(START_WIDTH as u32, START_HEIGHT as u32));
 
-    // let mut image = mq::Image::gen_image_color(START_WIDTH as u16, START_HEIGHT as u16, mq::BLACK);
-    // let render_to = mq::Texture2D::from_image(&image);
-
 
     let mut last_frame = instant::now();
     let mut delta: f32 = 1.0 / 60.0;
 
+
     let mut lights: Vec<Light> = Vec::new();
     lights.push(Light::new(Vector2D::new(300., 300.), 15., 1., mq::GRAY));
 
-    loop {
 
-        // let mut move_vec = Vector2D::new(0., 0.);
-        // if mq::is_key_down(mq::KeyCode::W) {
-        //     move_vec.y += 1.;
-        // }
-        // if mq::is_key_down(mq::KeyCode::S) {
-        //     move_vec.y -= 1.;
-        // }
-        // if mq::is_key_down(mq::KeyCode::A) {
-        //     move_vec.x -= 1.;
-        // }
-        // if mq::is_key_down(mq::KeyCode::D) {
-        //     move_vec.x += 1.;
-        // }
-        // lights[0].pt += move_vec;//.with_len(100. * delta);
+    loop {
+        // ------------------------------------------------------------------ //
+        let mut move_vec = Vector2D::new(0., 0.);
+        if mq::is_key_down(mq::KeyCode::W) {
+            move_vec.y -= 1.;
+        }
+        if mq::is_key_down(mq::KeyCode::S) {
+            move_vec.y += 1.;
+        }
+        if mq::is_key_down(mq::KeyCode::A) {
+            move_vec.x -= 1.;
+        }
+        if mq::is_key_down(mq::KeyCode::D) {
+            move_vec.x += 1.;
+        }
+        lights[0].pt += move_vec.with_len(100. * delta);
+        // ------------------------------------------------------------------ //
 
         mq::set_camera(&camera);
 
@@ -67,55 +70,47 @@ async fn main() {
             mq::GREEN,
         );
 
-
         for light in lights.iter() {
-            mq::draw_circle(light.pt.x, light.pt.y, 20., light.color);
+            mq::draw_circle(light.pt.x, light.pt.y, 20., mq::YELLOW);
         }
 
+        mq::set_camera(&mq::Camera2D::from_display_rect(mq::Rect::new(0., 0., START_WIDTH as f32, START_HEIGHT as f32)));
 
-        // mq::get_screen_data();
-        // let d = screen_image.get_image_data();
-        // println!("{}", d.len());
+        let image_in = camera.render_target.unwrap().texture.get_texture_data();
+        let mut image_out = mq::Image::gen_image_color(START_WIDTH as u16, START_HEIGHT as u16, mq::BLACK);
 
-        // for i in 0..START_WIDTH * START_HEIGHT {
-        //     let x = i % START_WIDTH;
-        //     let y = i / START_WIDTH;
-        //     for light in lights.iter() {
-        //         let dx = light.pt.x as i32 - x; 
-        //         let dy = light.pt.y as i32 - y;
-        //         let dist = ((dx * dx + dy * dy) as f32).sqrt();
+        for i in 0..START_WIDTH * START_HEIGHT {
+            let x = i % START_WIDTH;
+            let y = i / START_WIDTH;
 
-        //         if dist < light.power * 2.
-        //             || dist / light.power
-        //                 <= DITHER
-        //                     [(((dy.unsigned_abs() % 4) * 4) + (dx.unsigned_abs() % 4)) as usize]
-        //                     as f32
-        //         {
-        //             let screen_px_color = screen_image.get_pixel(x as u32, y as u32);
-        //             image.set_pixel(x as u32, y as u32, if screen_px_color == mq::BLACK {
-        //                 light.color
-        //             } else {
-        //                 screen_px_color
-        //             });
+            for light in lights.iter() {
+                let dx = light.pt.x as i32 - x; 
+                let dy = light.pt.y as i32 - y;
+                let dist = ((dx * dx + dy * dy) as f32).sqrt();
 
-        //             break;
-        //         } else {
-        //             image.set_pixel(x as u32, y as u32, mq::BLACK);
-        //         }
-        //     }
-        // }
+                if dist < light.power * 2.
+                    || dist / light.power
+                        <= DITHER
+                            [(((dy.unsigned_abs() % DITHER_SIZE) * DITHER_SIZE) + (dx.unsigned_abs() % DITHER_SIZE)) as usize]
+                            as f32
+                {
+                    let screen_px_color = image_in.get_pixel(x as u32, y as u32);
+                    image_out.set_pixel(x as u32, y as u32, if screen_px_color == mq::BLACK {
+                        light.color
+                    } else {
+                        screen_px_color
+                    });
+
+                    break;
+                }
+            }
+        }
+        // overwrite the texture with the new image
+        camera.render_target.unwrap().texture.update(&image_out);
+
 
         delta = (instant::now() - last_frame) as f32 / 1000.;
         last_frame = instant::now();
-
-        // texture.update(&image);
-        // mq::draw_texture_ex(texture, 0., 0., mq::WHITE, mq::DrawTextureParams {
-        //     dest_size: Some(mq::vec2(START_WIDTH as f32, START_HEIGHT as f32)),
-        //     flip_y: true,
-        //     ..Default::default()
-        // });
-
-        mq::set_camera(&mq::Camera2D::from_display_rect(mq::Rect::new(0., 0., START_WIDTH as f32, START_HEIGHT as f32)));
 
         mq::draw_texture_ex(
             camera.render_target.unwrap().texture,
